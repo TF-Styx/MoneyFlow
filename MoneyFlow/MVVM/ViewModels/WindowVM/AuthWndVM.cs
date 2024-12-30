7f25c7b32a27a0a283c5b01817fc6f44d358b759
texno stalin <texno.stalin@gmail.com>
2024-12-30 21:30:05 +0300

    Создали окно авторизации

---
+++ MoneyFlow/MVVM/ViewModels/WindowVM/AuthWndVM.cs
@@ -0,0 +1,19 @@
+﻿using MoneyFlow.MVVM.ViewModels.BaseVM;
+using MoneyFlow.Utils.Services.NavigationServices;
+
+namespace MoneyFlow.MVVM.ViewModels.WindowVM
+{
+    public class AuthWndVM : BaseViewModel, IUpdatable
+    {
+        private IServiceProvider _serviceProvider;
+
+        public AuthWndVM(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void Update(object parameter)
+        {
+            
+        }
+    }
+}
---
+++ MoneyFlow/MVVM/ViewModels/WindowVM/MainWindowVM.cs
@@ -0,0 +1,24 @@
+﻿using MoneyFlow.MVVM.ViewModels.BaseVM;
+using MoneyFlow.Utils.Services.NavigationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.MVVM.ViewModels.WindowVM
+{
+    public class MainWindowVM : BaseViewModel, IUpdatable
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public MainWindowVM(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void Update(object parameter)
+        {
+            
+        }
+    }
+}
