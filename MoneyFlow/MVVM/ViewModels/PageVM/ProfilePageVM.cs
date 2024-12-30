a4c7b08578205f46aa2eb85e61d77ece382168a4
texno stalin <texno.stalin@gmail.com>
2024-12-30 21:30:46 +0300

    Создали страницу профиля

---
+++ MoneyFlow/MVVM/ViewModels/PageVM/ProfilePageVM.cs
@@ -0,0 +1,20 @@
+﻿using MoneyFlow.MVVM.ViewModels.BaseVM;
+using MoneyFlow.Utils.Services.NavigationServices;
+
+namespace MoneyFlow.MVVM.ViewModels.PageVM
+{
+    internal class ProfilePageVM : BaseViewModel, IUpdatable
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ProfilePageVM(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Update(object parameter)
+        {
+            
+        }
+    }
+}
