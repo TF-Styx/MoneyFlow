06cfa69abd1eb1a0e569056e56c4b72a0958f18a
texno stalin <texno.stalin@gmail.com>
2025-01-22 17:49:50 +0300

    Переделана база данных

---
+++ MoneyFlow/MVVM/Models/DB_MSSQL/Subcategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyFlow.MVVM.Models.DB_MSSQL;
+
+public partial class Subcategory
+{
+    public int IdSubcategory { get; set; }
+
+    public string SubcategoryName { get; set; }
+
+    public string Description { get; set; }
+
+    public byte[] Image { get; set; }
+
+    public int IdCategory { get; set; }
+
+    public virtual Category IdCategoryNavigation { get; set; }
+}
