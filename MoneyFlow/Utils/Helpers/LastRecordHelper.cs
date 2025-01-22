c77389ab7b7914c0b83d11e587100d48a5cb6391
texno stalin <texno.stalin@gmail.com>
2025-01-22 17:51:57 +0300

    Обновление последнего элемента в списках

--- MoneyFlow/Utils/Helpers/LastRecordHelper.cs
+++ MoneyFlow/Utils/Helpers/LastRecordHelper.cs
@@ -1,4 +1,4 @@
-﻿using MoneyFlow.MVVM.Models.MSSQL_DB;
+﻿using MoneyFlow.MVVM.Models.DB_MSSQL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,17 +7,50 @@ using System.Threading.Tasks;
 
 namespace MoneyFlow.Utils.Helpers
 {
-    public class LastRecordHelper(Func<MoneyFlowContext> context)
+    public class LastRecordHelper(Func<MoneyFlowDbContext> context)
     {
-        private readonly Func<MoneyFlowContext> _context = context;
+        private readonly Func<MoneyFlowDbContext> _context = context;
 
         public FinancialRecord LastRecordFinancialRecord(User user)
         {
-            using (MoneyFlowContext context = _context())
+            using (MoneyFlowDbContext context = _context())
             {
                 return context.FinancialRecords
                     .Where(x => x.IdCategoryNavigation.IdUser == user.IdUser)
                         .OrderByDescending(x => x.IdFinancialRecord)
+                            .FirstOrDefault();
+            }
+        }
+
+        public Category LastRecordCategory(User user)
+        {
+            using (MoneyFlowDbContext context = _context())
+            {
+                return context.Categories
+                    .Where(x => x.IdUser == user.IdUser)
+                        .OrderByDescending(x => x.IdCategory)
+                            .FirstOrDefault();
+            }
+        }
+
+        public Subcategory LastRecordSubcategory(Category category)
+        {
+            using (MoneyFlowDbContext context = _context())
+            {
+                return context.Subcategories
+                    .Where(x => x.IdCategory == category.IdCategory)
+                        .OrderByDescending(x => x.IdCategory)
+                            .FirstOrDefault();
+            }
+        }
+
+        public Account LastRecordAccount(User user)
+        {
+            using (MoneyFlowDbContext context = _context())
+            {
+                return context.Accounts
+                    .Where(x => x.IdUser == user.IdUser)
+                        .OrderByDescending(x => x.IdAccount)
                             .FirstOrDefault();
             }
         }
