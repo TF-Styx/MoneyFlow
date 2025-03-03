c1f3697a67dd30ced68398c083e95ac81c9240f3
texno stalin <texno.stalin@gmail.com>
2025-03-04 01:18:39 +0300

    Базовое представление Профиля

--- MoneyFlow.Domain/DomainModels/UserBanksDomain.cs
+++ MoneyFlow.Domain/DomainModels/UserBanksDomain.cs
@@ -2,7 +2,14 @@
 {
     public class UserBanksDomain
     {
-        public int IdUser { get; set; }
-        public List<BankDomain> Banks { get; set; } = new List<BankDomain>();
+        public int IdUser { get; set; } // private set;
+        public List<BankDomain> Banks { get; set; } = new List<BankDomain>(); // private set;
+
+        public void AddNewBank(BankDomain bank)
+        {
+            if (string.IsNullOrWhiteSpace(bank.BankName)) { throw new Exception("Наименование банка отсутствует!!"); }
+
+            Banks.Add(bank);
+        }
     }
 }
